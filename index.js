3bfee9f9aed3ffc4fe60610cde9815d52109e6b2
ismaildevv@gmail.com <ismaildevv@gmail.com>
2023-03-10 20:39:53 +0100

    Capruting post data and redirecting functionality

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+let urls = [];
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.get('/api/shorturl/:id', function(req, res) {
+  if(!urls[req.params.id]) res.redirect('/')
+  res.redirect(urls[req.params.id])
+});
+
+app.post('/api/shorturl', function(req, res) {
+  urls.push(req.body.url);
+  res.redirect("/")
+  console.log(urls);
 });
 
 app.listen(port, function() {
